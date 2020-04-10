26d17e6354becdd2d8112627be5f96c86182c7a3
tonybricheff <tony.bricheff@yandex.ru>
2020-04-10 14:56:10 +0300

    interfaces added

---
+++ src/ru/brichev/runner/interfaces/Processor.java
@@ -0,0 +1,28 @@
+package ru.brichev.runner.interfaces;
+
+import ru.brichev.runner.modules.ProcessorException;
+
+import java.util.List;
+
+public interface Processor<T> {
+
+    /**
+     * @return processor id, immutable, unique among all instances, not null
+     */
+    String getId();
+
+    /**
+     * @return a list of processors that have to be executed before this one
+     *         and whose results must be passed to Processor::process,
+     *         immutable, can be null or empty, both means no inputs
+     */
+    List<String> getInputIds();
+
+    /**
+     * @param input outputs of the processors whose ids are returned by Processor::getInputIds, not null, but can be empty
+     * @return output of the processing, null if no output is produced
+     * @throws ProcessorException if error occurs during processing
+     */
+    T process(List<T> input) throws ProcessorException;
+
+}
